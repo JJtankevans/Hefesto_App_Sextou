d01488e01b668099f60cc25d274c835351882b5e
Vitor Oliveira <vitorflamengao2@gmail.com>
2020-11-26 18:59:22 -0400

    trasição de tela adicionada

--- app/src/main/java/com/example/sextou_app/MainActivity.kt
+++ app/src/main/java/com/example/sextou_app/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.sextou_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun iniciaCadastro (view: View){
+
+        val btn_cadastrar = findViewById<Button>(R.id.button2)
+        btn_cadastrar.setOnClickListener {
+            val intent = Intent(this, cadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
