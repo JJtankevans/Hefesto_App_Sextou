4902dd0cbb66f48275e7a63a19d9ae7f5da4d9f0
Vitor Oliveira <vitorflamengao2@gmail.com>
2020-11-27 01:49:54 -0400

    Telas de cadastro

--- app/src/main/java/com/example/sextou_app/cadastroActivity.kt
+++ app/src/main/java/com/example/sextou_app/cadastroActivity.kt
@@ -1,11 +1,32 @@
 package com.example.sextou_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
 
 class cadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro)
     }
+
+    fun cadastraArtista(view: View){
+        val btn_Artist = findViewById<Button>(R.id.button3)
+        btn_Artist.setOnClickListener {
+            val intent = Intent(this, CadartistiActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    fun cadastraSexto(view: View){
+        val btn_Sexto = findViewById<Button>(R.id.button4)
+        btn_Sexto.setOnClickListener {
+            val intent = Intent(this, CadsextoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
